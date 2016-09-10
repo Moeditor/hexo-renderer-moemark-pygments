5111a43ae3c9b2c720cb5c6085b22b161d8df36b
Menci <HuangHaoRui301@Gmail.com>
2016-09-10 14:27:29 +0800

    Use MoeMark

--- index.js
+++ index.js
@@ -3,22 +3,11 @@
 'use strict';
 
 var renderer = require('./lib/renderer');
-var assign = require('object-assign');
 
-hexo.config.marked = assign({
-  gfm: true,
-  pedantic: false,
-  sanitize: false,
-  tables: true,
-  breaks: true,
-  smartLists: true,
-  smartypants: true
-}, hexo.config.marked);
-
-hexo.extend.renderer.register('md', 'html', renderer, true);
-hexo.extend.renderer.register('markdown', 'html', renderer, true);
-hexo.extend.renderer.register('mkd', 'html', renderer, true);
-hexo.extend.renderer.register('mkdn', 'html', renderer, true);
-hexo.extend.renderer.register('mdwn', 'html', renderer, true);
-hexo.extend.renderer.register('mdtxt', 'html', renderer, true);
-hexo.extend.renderer.register('mdtext', 'html', renderer, true);
+hexo.extend.renderer.register('md', 'html', renderer);
+hexo.extend.renderer.register('markdown', 'html', renderer);
+hexo.extend.renderer.register('mkd', 'html', renderer);
+hexo.extend.renderer.register('mkdn', 'html', renderer);
+hexo.extend.renderer.register('mdwn', 'html', renderer);
+hexo.extend.renderer.register('mdtxt', 'html', renderer);
+hexo.extend.renderer.register('mdtext', 'html', renderer);
